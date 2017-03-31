e561930a3bdfeeb3a3efce9dabdf8d05afb507b5
Elliot Humphrey <evhumphrey@users.noreply.github.com>
2017-03-30 22:09:17 -0700

    Implement Map#assign(key, value), returning the [key, value] assigned or updated

--- W1D5/map.rb
+++ W1D5/map.rb
@@ -1,10 +1,20 @@
 # map = [[k1, v1], [k2, v2], [k3, v3]]
 class Map
   def initialize
+    @map = []
   end
 
   def assign(key, value)
     # can create a new [key, value] pair or update val for existing key
+    @map.each do |pair|
+      if pair.first == key
+        pair[-1] = value
+        return [key, value]
+      end
+    end
+
+    @map << [key, value]
+    [key, value]
   end
 
   def lookup(key)

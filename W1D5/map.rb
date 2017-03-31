1738f00ff424dd9bcfd766819c3407aa3e69358e
Elliot Humphrey <evhumphrey@users.noreply.github.com>
2017-03-30 21:52:27 -0700

    Add Map class skeleton

---
+++ W1D5/map.rb
@@ -0,0 +1,20 @@
+# map = [[k1, v1], [k2, v2], [k3, v3]]
+class Map
+  def initialize
+  end
+
+  def assign(key, value)
+    # can create a new [key, value] pair or update val for existing key
+  end
+
+  def lookup(key)
+    # find the value at specified key
+  end
+
+  def remove(key)
+    # remove the [key, value] pair
+  end
+
+  def show
+  end
+end

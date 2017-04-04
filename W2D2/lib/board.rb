6affdd5fd97f8f7607bbff854aaffa1c761d20ed
Elliot Humphrey <evhumphrey@users.noreply.github.com>
2017-04-03 20:07:18 -0700

    Implement Board#initialize

--- W2D2/lib/board.rb
+++ W2D2/lib/board.rb
@@ -2,10 +2,20 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
+
+    @cups = Array.new(14) { [] }
+    place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    @cups.map.with_index do |cup, cup_idx|
+      next if cup_idx == 6 || cup_idx == 13
+
+      cup.concat([:stone, :stone, :stone, :stone])
+    end
   end
 
   def valid_move?(start_pos)

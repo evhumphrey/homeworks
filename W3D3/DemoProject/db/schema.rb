1785d743ddbf950ad74cb93b99a8e3624c899634
Elliot Humphrey <evhumphrey@users.noreply.github.com>
2017-04-11 21:31:21 -0700

    Add House table and House model

--- W3D3/DemoProject/db/schema.rb
+++ W3D3/DemoProject/db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170412035741) do
+ActiveRecord::Schema.define(version: 20170412042301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "houses", force: :cascade do |t|
+    t.string   "address",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.string   "name",       null: false
--- W3D3/DemoProject/db/seeds.rb
+++ W3D3/DemoProject/db/seeds.rb
@@ -6,3 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Person.create(name: 'Default Dan', house_id: 1)
+Person.create(name: 'Plain Jane', house_id: 1)
+House.create(address: '1234 Normal Street')
+
+Person.create(name: 'Lonesome Tom', house_id: 2)
+House.create(address: '4321 Lonely Avenue')

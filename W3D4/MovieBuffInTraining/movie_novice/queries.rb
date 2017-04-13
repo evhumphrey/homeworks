4519dc6fa071f355d10ff11e91c51655678e4d3c
Elliot Humphrey <evhumphrey@users.noreply.github.com>
2017-04-12 21:34:25 -0700

    Pass #pulp_fiction_actors

--- W3D4/MovieBuffInTraining/movie_novice/queries.rb
+++ W3D4/MovieBuffInTraining/movie_novice/queries.rb
@@ -69,7 +69,10 @@ def pulp_fiction_actors
   # practice using joins
   # display the id and name of all actors in the movie Pulp Fiction
   # hint: use 'select', 'joins', 'where'
-
+  Movie
+    .select("actors.id, actors.name")
+    .joins(:actors)
+    .where("movies.title = ?", "Pulp Fiction")
 end
 
 def uma_movies

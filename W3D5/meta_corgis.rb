d11e785e7d483a84e358ac791c104fd602a1aad2
Elliot Humphrey <evhumphrey@users.noreply.github.com>
2017-04-14 00:17:25 -0700

    Implement MetaCorgiSnacks::define_snack

--- W3D5/meta_corgis.rb
+++ W3D5/meta_corgis.rb
@@ -107,21 +107,28 @@ class MetaCorgiSnacks
   def initialize(snack_box, box_id)
     @snack_box = snack_box
     @box_id = box_id
+
+    names = snack_box.methods.grep(/^get_(.*)_info$/) { MetaCorgiSnacks.define_snack $1 }
+    names.each { |name| self.class.define_snack(name) }
   end
 
-  def method_missing(name, *args)
-    if [:kibble, :treat, :bone].include?(name)
+  # def method_missing(name, *args)
+  #   if [:kibble, :treat, :bone].include?(name)
+  #     info = @snack_box.send("get_#{name}_info", @box_id)
+  #     tastiness = @snack_box.send("get_#{name}_tastiness", @box_id)
+  #     result = "Treat: #{info}: #{tastiness} "
+  #     tastiness > 30 ? "* #{result}" : result
+  #   else
+  #     super
+  #   end
+  # end
+
+  def self.define_snack(name)
+    define_method(name) do
       info = @snack_box.send("get_#{name}_info", @box_id)
       tastiness = @snack_box.send("get_#{name}_tastiness", @box_id)
       result = "Treat: #{info}: #{tastiness} "
       tastiness > 30 ? "* #{result}" : result
-    else
-      super
     end
-  end
-
-
-  def self.define_snack(name)
-    # Your code goes here...
   end
 end

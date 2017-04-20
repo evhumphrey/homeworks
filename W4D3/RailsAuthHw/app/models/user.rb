9e52273cf8abd04879ac2d66674738a1ae27b092
Elliot Humphrey <evhumphrey@users.noreply.github.com>
2017-04-19 19:53:27 -0700

    Implement Phase 3 and 4:

--- W4D3/RailsAuthHw/app/models/user.rb
+++ W4D3/RailsAuthHw/app/models/user.rb
@@ -3,5 +3,31 @@ class User < ActiveRecord::Base
   validates :password_digest, presence: { message: "Password can't be blank" }
   validates :password, length: { minimum: 6 }, allow_nil: true
 
-  before_validation :ensure_session_token 
+  before_validation :ensure_session_token
+  attr_reader :password
+  def self.find_by_credentials(username, password)
+    user = User.find_by(:username)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
 end

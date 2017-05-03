ca4ab7e797e0e588e48379d5a90228a635084d08
Elliot Humphrey <evhumphrey@users.noreply.github.com>
2017-05-02 23:28:58 -0700

    simple ajax

--- W6D3/ajax/js/index.js
+++ W6D3/ajax/js/index.js
@@ -1,5 +1,17 @@
 console.log("Hello from the JavaScript console!");
 
-// Your AJAX request here
+// Your AJAX request here (note success and error are functions)
+$.ajax({
+  type: 'GET',
+  url: 'http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=bcb83c4b54aee8418983c2aff3073b3b',
+  success(data) {
+    console.log("We have your weather!");
+    console.log(data);
+  },
+  error() {
+    console.error("An error has occured.");
+  }
+});
 
 // Add another console log here, outside your AJAX request
+console.log("I'm below the AJAX request (in the js file)");

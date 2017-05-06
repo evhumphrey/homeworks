7bc0f608675a54a46dae33ee645213c22f6e6c26
Elliot Humphrey <evhumphrey@users.noreply.github.com>
2017-05-06 16:43:55 -0700

    Add toggle and add image functionality

--- W6D4/skeleton/script.js
+++ W6D4/skeleton/script.js
@@ -20,6 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const addToList = (e) => {
     e.preventDefault();
+
     const placesList = document.querySelector("#sf-places");
     const input = document.querySelector(".favorite-input");
     const newPlace = input.value;
@@ -33,9 +34,37 @@ document.addEventListener("DOMContentLoaded", () => {
   addButton.addEventListener("click", addToList);
 
   // adding new photos
+  const togglePhotoForm = (e) => {
+    e.preventDefault();
 
-  
+    const form = document.querySelector(".photo-form-container");
+    console.log(form);
+    form.classList.toggle("hidden");
+  };
 
+  const togglePhotoFormButton = document.querySelector(".photo-show-button");
+  togglePhotoFormButton.addEventListener("click", togglePhotoForm);
+
+  const addPhoto = (e) => {
+    e.preventDefault();
+
+    const urlInput = document.querySelector(".photo-url-input");
+    const newPhotoUrl = urlInput.value;
+    urlInput.value = "";
+
+    const photoList = document.querySelector(".dog-photos");
+    const newLi = document.createElement("li");
+    const newPhoto = document.createElement("img");
+    newPhoto.setAttribute('src', newPhotoUrl);
+
+
+
+    newLi.appendChild(newPhoto);
+    photoList.appendChild(newLi);
+  };
+
+  const addPhotoButton = document.querySelector(".photo-url-submit");
+  addPhotoButton.addEventListener("click", addPhoto);
 
 
 });
